ca3f5d7e76b11b8679fcaa24730fe969263c5008
Brad Pickering-Dunn <brad@pickering-dunn.com>
2016-06-23 19:13:31 +0100

    Test project added with basic test to validate current funkynality.  Add customer cant be checked as not all classes can be faked out

---
+++ App.Tests/CustomerServiceTestFixture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Tests
+{
+    [TestClass]
+    public class CustomerServiceTestFixture
+    {
+        [TestMethod]
+        public void Ensure_A_Customer_Can_Be_Added()
+        {
+
+            var result = new CustomerService().AddCustomer("a", "b", "@.v", DateTime.Now.AddYears(-22), 1);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Ensure_A_Customer_Cant_Be_Added_If_Firstname_Invalid()
+        {
+
+            var result = new CustomerService().AddCustomer("", "b", "@.v", DateTime.Now.AddYears(-22), 1);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Ensure_A_Customer_Cant_Be_Added_If_Email_Invalid()
+        {
+
+            var result = new CustomerService().AddCustomer("a", "", "@.v", DateTime.Now.AddYears(-22), 1);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Ensure_A_Customer_Cant_Be_Added_If_Lastname_Invalid()
+        {
+
+            var result = new CustomerService().AddCustomer("a", "", ".v", DateTime.Now.AddYears(-22), 1);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Ensure_A_Customer_Cant_Be_Added_If_Age_Invalid()
+        {
+
+            var result = new CustomerService().AddCustomer("a", "a", ".v", DateTime.Now, 1);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
